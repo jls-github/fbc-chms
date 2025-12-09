23bf2be5485332491f998256fccf707c534d5699
John Souza <souzajohnlee@gmail.com>
2025-12-08 23:56:45 -0800

    add some ux

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,7 @@ class DashboardController < ApplicationController
     @member_count = Member.count
     @group_member_count = GroupMember.pluck(:member_id).uniq.count
     @team_member_count = TeamMember.pluck(:member_id).uniq.count
+    @members_without_group = Member.left_joins(:group_members).where(group_members: { id: nil }).map(&:full_name).join(", ")
+    @members_without_team = Member.left_joins(:team_members).where(team_members: { id: nil }).map(&:full_name).join(", ")
   end
 end

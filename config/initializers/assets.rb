813b8f02203161153c81be53b521232b1d40c7dc
John Souza <souzajohnlee@gmail.com>
2025-12-16 13:58:13 -0800

    remove npm stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,5 +2,3 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.1"
-
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
